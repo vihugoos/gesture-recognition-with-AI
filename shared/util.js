151bfb86921a44e29fcdc734994608fc43ced41a
Victor Hugo <victorhugoos@live.com>
2023-03-08 20:47:32 -0300

    feat(shared/util): functions supportsWorkerType and prepareRunChecker created

---
+++ shared/util.js
@@ -0,0 +1,29 @@
+export function supportsWorkerType() {
+    let supports = false;
+
+    const tester = {
+        get type() {
+            supports = true;
+        },
+    };
+
+    try {
+        new Worker("blob://", tester).terminate();
+    } finally {
+        return supports;
+    }
+}
+
+export function prepareRunChecker({ timerDelay }) {
+    let lastEvent = Date.now();
+
+    return {
+        shouldRun() {
+            const result = (Date.now() - lastEvent) > timerDelay;
+
+            if (result) lastEvent = Date.now();
+
+            return result;
+        },
+    };
+}
