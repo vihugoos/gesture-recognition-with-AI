7f5bc3f417574c62e317e1a523b2a6c1eb20d40c
Victor Hugo <victorhugoos@live.com>
2023-03-11 03:35:03 -0300

    feat(handGestureView): created functions for drawing hands and clicking on elements

--- pages/titles/src/views/handGestureView.js
+++ pages/titles/src/views/handGestureView.js
@@ -1,4 +1,18 @@
 export default class HandGestureView {
+    #handsCanvas = document.querySelector("#hands");
+    #canvasContext = this.#handsCanvas.getContext("2d");
+    #fingerLookupIndexes;
+
+    constructor({ fingerLookupIndexes }) {
+        this.#handsCanvas.width = globalThis.screen.availWidth;
+        this.#handsCanvas.height = globalThis.screen.availHeight;
+        this.#handsCanvas.style.position = "fixed";
+        this.#handsCanvas.style.zIndex = 1000;
+        this.#handsCanvas.style.pointerEvents = "none";
+
+        this.#fingerLookupIndexes = fingerLookupIndexes;
+    }
+
     loop(fn) {
         requestAnimationFrame(fn);
     }
@@ -8,5 +22,82 @@ export default class HandGestureView {
             top,
             behavior: "smooth",
         });
+    }
+
+    clearCanvas() {
+        this.#canvasContext.clearRect(
+            0,
+            0,
+            this.#handsCanvas.width,
+            this.#handsCanvas.height
+        );
+    }
+
+    #drawJoients(keypoints) {
+        for (const { x, y } of keypoints) {
+            this.#canvasContext.beginPath();
+
+            const newX = x - 2;
+            const newY = y - 2;
+            const radius = 3;
+            const startAngle = 0;
+            const endAngle = 2 * Math.PI;
+
+            this.#canvasContext.arc(newX, newY, radius, startAngle, endAngle);
+            this.#canvasContext.fill();
+        }
+    }
+
+    drawResults(hands) {
+        for (const { keypoints } of hands) {
+            if (!keypoints) continue;
+
+            this.#canvasContext.fillStyle = "rgb(44, 212, 103)";
+            this.#canvasContext.strokeStyle = "white";
+            this.#canvasContext.lineWidth = 8;
+            this.#canvasContext.lineJoin = "round";
+
+            this.#drawJoients(keypoints);
+            this.#drawFingersAndHoverElements(keypoints);
+        }
+    }
+
+    #drawFingersAndHoverElements(keypoints) {
+        const fingers = Object.keys(this.#fingerLookupIndexes);
+
+        for (const finger of fingers) {
+            const points = this.#fingerLookupIndexes[finger].map(
+                (index) => keypoints[index]
+            );
+
+            const region = new Path2D();
+
+            const { x, y } = points;
+
+            region.moveTo(x, y);
+
+            for (const point of points) {
+                region.lineTo(point.x, point.y);
+            }
+
+            this.#canvasContext.stroke(region);
+        }
+    }
+
+    clickOnElement(x, y) {
+        const element = document.elementFromPoint(x, y);
+        if (!element) return;
+
+        const rect = element.getBoundingClientRect();
+
+        const event = new MouseEvent("click", {
+            view: window,
+            bubbles: true,
+            cancelable: true,
+            clientX: rect.left + x,
+            clientY: rect.top + y,
+        });
+
+        element.dispatchEvent(event);
     }
 }
