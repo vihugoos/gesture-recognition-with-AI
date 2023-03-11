25e916a74d78f633a5080b205e86d2d4ec7f4fdd
Victor Hugo <victorhugoos@live.com>
2023-03-11 03:37:44 -0300

    feat(handGestureController): implementing drawing of hands on screen

--- pages/titles/src/controllers/handGestureController.js
+++ pages/titles/src/controllers/handGestureController.js
@@ -1,6 +1,7 @@
 import { prepareRunChecker } from "../../../../shared/util.js";
 
 const { shouldRun: scrollShouldRun } = prepareRunChecker({ timerDelay: 200 });
+const { shouldRun: clickShouldRun } = prepareRunChecker({ timerDelay: 300 });
 
 export default class HandGestureController {
     #view;
@@ -51,9 +52,21 @@ export default class HandGestureController {
     async #estimateHands() {
         try {
             const hands = await this.#service.estimateHands(this.#camera.video);
+
+            this.#view.clearCanvas();
+
+            if (hands?.length) this.#view.drawResults(hands);
+
             for await (const { event, x, y } of this.#service.detectGestures(
                 hands
             )) {
+                if (event === "click") {
+                    if (!clickShouldRun()) continue;
+
+                    this.#view.clickOnElement(x, y);
+                    continue;
+                }
+
                 if (event.includes("scroll")) {
                     if (!scrollShouldRun) continue;
 
